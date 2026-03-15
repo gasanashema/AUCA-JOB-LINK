f80b13272eeeadeb5b592af0fd371e10c5e079cc
Shema <shemaphilbert8@gmail.com>
2026-03-15 10:07:27 +0200

    feat: deploy configs implemented

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
   console.log("✅ MongoDB connected");
   seedJobs(); // Seed jobs after DB connection
